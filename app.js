b0333ac6b1b401c8c524a52461923c59bacda5bb
Andrew Heim <andrewsheim@gmail.com>
2022-11-07 12:26:22 -0800

    Completed GroceryList with Basic Requirements

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// TODO
+
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+
+  const [isDone, setIsDone] = useState(false);
+
+  const style = {
+    fontWeight: isDone ? 'bold' : 'normal'
+  };
+  return (
+    <li style={style}
+      onMouseEnter={() => setIsDone(true)}
+      onMouseLeave={() => setIsDone(false)}>
+      {props.groceries}
+    </li>
+  );
+};
+
+const GroceryList = (props) => {
+
+  const onListItemClick = (event) => {
+    console.log('clicked bruh');
+  };
+
+  return (
+    <ul>
+      {props.groceries.map((grocery) => (
+        <GroceryListItem groceries={grocery} />
+      ))}
+    </ul>
+  );
+}
+
+
+const App = () => (
+  <div>
+    <h1>Grocery List</h1>
+    <GroceryList groceries={['avocado', 'banana', 'protein powder']}/>
+  </div>
+);
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
